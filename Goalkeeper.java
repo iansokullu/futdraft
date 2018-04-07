3e0a7d07dac9a641e4d85fad1b8ffb0a4960e317
iansokullu <bobpiero50@gmail.com>
2018-04-07 11:49:35 -0400

    Delete Goalkeeper.java

--- Goalkeeper.java
+++
@@ -1,8 +0,0 @@
-public class Goalkeeper extends Player {
-    public Goalkeeper(int r, int i, String c, String cl, String l, String ca) {
-        super(r, i, c, cl, l, ca);
-    }
-    public Goalkeeper() {
-        super();
-    }
-}
